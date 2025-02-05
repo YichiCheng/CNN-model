faecfc60ee1f9493f59aea31fbb286790ce3d23a
YichiCheng <yic04642@gmail.com>
2025-02-04 20:06:14 -0800

    Update train1c.py

--- train1c.py
+++ train1c.py
@@ -16,7 +16,7 @@ from tensorflow.keras.layers import MultiHeadAttention, LayerNormalization
 # Constants
 IMAGE_DIR = "C:/image_data/image_data/"
 LOG_FILE = "C:/logging_data/log_file_4.txt"  
-LOG_FILE_PATH = "C:/yichi/AVLpplogs/training1_log.txt" #training process log
+LOG_FILE_PATH = "C:/yichi/AVLpplogs/training2_log.txt" #training process log
 IMG_HEIGHT, IMG_WIDTH = 66, 200  
 BATCH_SIZE = 32
 EPOCHS = 50
@@ -157,7 +157,7 @@ def main():
             callbacks=[early_stopping, reduce_lr]
         )
     
-        model.save("steering_model_augmented_1.keras", save_format="keras")
+        model.save("steering_model_augmented_2.keras", save_format="keras")
         
     print("Model saved as steering_model_augmented.keras")
 
